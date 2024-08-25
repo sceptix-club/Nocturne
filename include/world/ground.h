73b90246443dcf353034dadfb9917cf022346cfc
septicalls <vyasamnayak@@gmail.com>
2024-08-25 18:04:26 +0530

    fixed file path bug

--- include/world/ground.h
+++ include/world/ground.h
@@ -10,7 +10,7 @@ Model Ground()
     Mesh floor = GenMeshPlane(32.0f, 32.0f, 1, 1);
     Model ground = LoadModelFromMesh(floor);
 
-    Texture2D grassfloor = LoadTexture("/assets/ground.png");
+    Texture2D grassfloor = LoadTexture("assets/ground.png");
     ground.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = grassfloor;
 
     return ground;
