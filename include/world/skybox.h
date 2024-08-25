3fbb909c9c594cc97b25fdb60ed34a90763a0cf9
nearlynithin <nearlynithin@gmail.com>
2024-08-25 21:14:03 +0530

    Setup skybox loading

--- include/world/skybox.h
+++ include/world/skybox.h
@@ -13,19 +13,14 @@ Model SkyBox(){
     Mesh cube = GenMeshCube(1.0f, 1.0f, 1.0f);
     Model skybox = LoadModelFromMesh(cube);
 
-    // static TextureCubemap GenTextureCubemap(Shader shader, Texture2D panorama, int size, int format);
-
-    skybox.materials[0].shader = LoadShader(TextFormat("resources/glsl%i/skybox.fs",GLSL_VERSION),TextFormat("resources/glsl%i/skybox.fs",GLSL_VERSION));
+    skybox.materials[0].shader = LoadShader(TextFormat("resources/shaders/glsl%i/skybox.vs",GLSL_VERSION),TextFormat("resources/shaders/glsl%i/skybox.fs",GLSL_VERSION));
     SetShaderValue(skybox.materials[0].shader, GetShaderLocation(skybox.materials[0].shader,"environmentMap"), (int[1]){ MATERIAL_MAP_CUBEMAP }, SHADER_UNIFORM_INT);
     SetShaderValue(skybox.materials[0].shader, GetShaderLocation(skybox.materials[0].shader,"doGamma"), (int[1]){0}, SHADER_UNIFORM_INT);
     SetShaderValue(skybox.materials[0].shader, GetShaderLocation(skybox.materials[0].shader,"vflipped"), (int[1]){1}, SHADER_UNIFORM_INT);
-    Shader shdrCubemap = LoadShader(TextFormat("resources/glsl%i/skybox.fs", GLSL_VERSION), TextFormat("resources/glsl%i/cubemap.fs", GLSL_VERSION));
-    SetShaderValue(shdrCubemap, GetShaderLocation(shdrCubemap, "equirectangularMap"), (int[1]){ 0 }, SHADER_UNIFORM_INT);
 
-
-    Image img = LoadImage("assets/sky.png");
-    skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture = LoadTextureCubemap(img, CUBEMAP_LAYOUT_AUTO_DETECT);
+    Image img = LoadImage("assets/skybox_moon.png");
+    TextureCubemap cubeMaptexture = LoadTextureCubemap(img, CUBEMAP_LAYOUT_AUTO_DETECT);
     UnloadImage(img);
-
+    skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture = cubeMaptexture;
     return skybox;
 }
