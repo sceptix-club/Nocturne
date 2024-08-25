d5abfa3b31ac39b03ff06ee8e3279571bfc4b005
septicalls <vyasamnayak@@gmail.com>
2024-08-25 16:13:18 +0530

    moved all headers to include directory

--- include/world/ground.h
+++ include/world/ground.h
@@ -7,11 +7,13 @@
 
 Model Ground()
 {
-    Mesh floor = GenMestPlane(32.0f, 32.0f, 1, 1);
-    Model ground = LoadModelFromMesd(floor);
+    Mesh floor = GenMeshPlane(32.0f, 32.0f, 1, 1);
+    Model ground = LoadModelFromMesh(floor);
 
     Texture2D grassfloor = LoadTexture("/assets/ground.png");
     ground.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = grassfloor;
 
     return ground;
 }
+
+#endif
