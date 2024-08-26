b3bf852aa2106a8b29b6584b06aea30971cd3f3b
nearlynithin <nearlynithin@gmail.com>
2024-08-26 15:03:34 +0530

    header files for grass

---
+++ include/world/grass.h
@@ -0,0 +1,12 @@
+#ifndef GRASS_H
+#define GRASS_H
+
+#include <raylib.h>
+
+float Noise(float x, float y);
+Model GrassBlade(Shader lightShader);
+void InitGrass(Vector3 playerPos, Shader lightShader);
+void UpdateGrassPatches(Vector3 playerPos);
+void DrawGrassNew(Model grass);
+
+#endif
