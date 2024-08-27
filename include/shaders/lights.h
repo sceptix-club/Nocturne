54f4504d8ae92a58ea08cd5ad490d80725abc8ad
nearlynithin <nearlynithin@gmail.com>
2024-08-27 17:53:22 +0530

    Light ambience decreased

--- include/shaders/lights.h
+++ include/shaders/lights.h
@@ -15,7 +15,7 @@ Shader SetLights()
     Shader lightShader = LoadShader(TextFormat("resources/shaders/glsl%i/lighting.vs",GLSL_VERSION), TextFormat("resources/shaders/glsl%i/lighting.fs", GLSL_VERSION));
     lightShader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(lightShader, "viewPos");
     int ambientLoc = GetShaderLocation(lightShader, "ambient");
-    float attenuation = 0.05f;
+    float attenuation = 0.03f;
     int attenuationLoc = GetShaderLocation(lightShader, "attenuationLoc");
     SetShaderValue(lightShader, ambientLoc, (float[4]){ 0.05f, 0.075f, 0.07f, 1.0f }, SHADER_UNIFORM_VEC4);
     SetShaderValue(lightShader, attenuationLoc, &attenuation, SHADER_UNIFORM_FLOAT);
