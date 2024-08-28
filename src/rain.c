93c77f59cc0fdf2da419144281d7648638cba4e8
JoelVCrasta <22b03.joelc@sjec.ac.in>
2024-08-28 16:31:41 +0530

    Rain fall added and rain toggle for now

--- src/rain.c
+++ src/rain.c
@@ -4,11 +4,11 @@
 
 #define RAINDROP_COUNT 1000
 #define RAINDROP_SCALE (Vector3){1.0f, 1.0f, 1.0f}
+#define MAX_OFFSET 30.0f
 
 
 typedef struct RainDrop {
     Vector3 position;
-    float rotationY;
 } rain_drop;
 
 rain_drop rainDrops[RAINDROP_COUNT];
@@ -16,27 +16,52 @@ rain_drop rainDrops[RAINDROP_COUNT];
 Model Rain() {
     Mesh plane = GenMeshPlane(0.03f, 0.3f, 1, 1);
     Model rain = LoadModelFromMesh(plane);
+
     return rain;
 }
 
-void InitRain() {
-    for (int i = 0; i < RAINDROP_COUNT; i++) {
-        rainDrops[i].position.x = GetRandomValue(-20, 20);
-        rainDrops[i].position.y = GetRandomValue(5, 20);
-        rainDrops[i].position.z = GetRandomValue(-20, 20);
+// Helper function to wrap rain drops position around the camera
+static inline void WrapRainPosition(float *position, float min, float max) {
+    if (*position > max) {
+        *position = min;
+    } else if (*position < min) {
+        *position = max;
     }
 }
 
-void UpdateRain() {
+// Initialize rain drops positions
+void InitRain(Vector3 cameraPosition) {
     for (int i = 0; i < RAINDROP_COUNT; i++) {
-        rainDrops[i].position.y -= 0.1f;
+        rainDrops[i].position.x = GetRandomValue(cameraPosition.x - MAX_OFFSET, cameraPosition.x + MAX_OFFSET);
+        rainDrops[i].position.y = GetRandomValue(5, MAX_OFFSET);
+        rainDrops[i].position.z = GetRandomValue(cameraPosition.z - MAX_OFFSET, cameraPosition.z + MAX_OFFSET);
+    }
+}
+ 
+// Update rain drops position and wrap around the camera
+void UpdateRain(Vector3 cameraPosition, int rainCount) {
+    float maxX = cameraPosition.x + MAX_OFFSET;
+    float minX = cameraPosition.x - MAX_OFFSET;
+    float maxZ = cameraPosition.z + MAX_OFFSET;
+    float minZ = cameraPosition.z - MAX_OFFSET;
+
+    for (int i = 0; i < RAINDROP_COUNT; i++) {
+        WrapRainPosition(&rainDrops[i].position.x, minX, maxX);
+        WrapRainPosition(&rainDrops[i].position.z, minZ, maxZ);
+
+        rainDrops[i].position.y -= 0.3f;
+
         if (rainDrops[i].position.y < 0) {
-            rainDrops[i].position.y = GetRandomValue(5, 20);
+            rainDrops[i].position.y = GetRandomValue(20, MAX_OFFSET);
         }
     }
+
+
 }
 
-void DrawRain(Model rain) {
+void DrawRain(Model rain, Vector3 cameraPosition, int rainCount) {
+    UpdateRain(cameraPosition, rainCount);
+
     for (int i = 0; i < RAINDROP_COUNT; i++) {
         DrawModelEx(rain, rainDrops[i].position, (Vector3){1.0f, 0.0f, 0.0f}, 90.0f, RAINDROP_SCALE, DARKGRAY);
     }
