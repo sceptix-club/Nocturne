7de4e0ac7e732ae968469e4bab8dda2b21a8d379
JoelVCrasta <22b03.joelc@sjec.ac.in>
2024-08-28 15:11:43 +0530

    Rain drops as planes added (for now its just static)

---
+++ include/world/rain.h
@@ -0,0 +1,14 @@
+#ifndef RAIN_H
+#define RAIN_H
+
+#include <raylib.h>
+#include <raymath.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+Model Rain();
+void InitRain();
+void DrawRain(Model rain);
+
+#endif // RAIN_H
