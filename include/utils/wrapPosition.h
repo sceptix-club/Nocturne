cc5d0f3616cd75e02cd45adb7890663fda502e44
JoelVCrasta <22b03.joelc@sjec.ac.in>
2024-08-29 12:38:22 +0530

    apparently you can't have inline definations in header

--- include/utils/wrapPosition.h
+++ include/utils/wrapPosition.h
@@ -3,7 +3,7 @@
 
 #include <raylib.h>
 
-inline void CalculateMinMax(Vector3* cameraPosition, float* minX, float* maxX, float* minZ, float* maxZ, float MAX_OFFSET);
-inline void WrapPosition(float *position, float min, float max);
+void CalculateMinMax(Vector3* cameraPosition, float* minX, float* maxX, float* minZ, float* maxZ, float MAX_OFFSET);
+void WrapPosition(float *position, float min, float max);
 
 #endif // WRAP_H
