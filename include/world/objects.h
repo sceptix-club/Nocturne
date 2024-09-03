bc1cd33606e9cbf205e5a1b22d1374a4f615f246
JoelVCrasta <22b03.joelc@sjec.ac.in>
2024-09-03 15:36:02 +0530

    object spawn position randomized and move up animation

--- include/world/objects.h
+++ include/world/objects.h
@@ -1,8 +1,17 @@
 #ifndef OBJECTS_H
 #define OBJECTS_H
 
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
+#include <raylib.h>
+#include <raymath.h>
+#include <stdio.h>
+
+extern bool allObjectsFound;
+
+Model ObjectModel(Shader lightShader);
 void InitObjects();
-Model ObjectModel();
-bool DrawObjects();
+void DrawObjects(Model object, Camera *camera);
 
 #endif
--- src/objects.c
+++ src/objects.c
@@ -1,69 +1,91 @@
-#include <time.h>
-#include <math.h>
-#include <stdlib.h>
-#include <raylib.h>
-#include <raymath.h>
 #include "world/objects.h"
 
-#define GAMEPLAY_MAP_SIZE 500 // All four hidden objects will be within a circle of this radius
-#define MIN_SPAWN_DISTANCE 100
+#define MAP_SIZE 25 // All four hidden objects will be within a circle of this radius
+#define MIN_SPAWN_RADIUS 25
 #define DISTANCE_THRESHOLD 10.0f
-#define NUM_OBJECTS 4
+#define OBJECT_COUNT 4
+#define OBJECT_Y 10.0f
 
 typedef struct {
     Vector3 position;
     bool visible;
-} object_t;
+    bool isFound;
+} Object;
 
-object_t objects[NUM_OBJECTS];
+Object objects[OBJECT_COUNT];
+bool allObjectsFound = false;
 
-Model ObjectModel() {
+Model ObjectModel(Shader lightShader) {
     Model object = LoadModel("assets/models/sphere.obj");
 
-    // Create a default material
     Material material = LoadMaterialDefault();
-    // Set the diffuse colour to a bright, glowing colour
     material.maps[MATERIAL_MAP_DIFFUSE].color = (Color){ 255, 0, 0, 255 };
-
     object.materials[0] = material;
 
     return object;
 }
 
+// Initialize hidden objects at random positions in 4 quadrants
 void InitObjects() {
-    srand(time(NULL));
+    float mapQuadrant = MAP_SIZE / 2.0f;
+    float minQuadrant = MIN_SPAWN_RADIUS / 2.0f;
 
-    for (int i = 0; i < NUM_OBJECTS; i++) {
-        // Generate random polar cooridnates for each object
-        float angle = GetRandomValue(0, 359) * DEG2RAD;
-        float distance = GetRandomValue(MIN_SPAWN_DISTANCE, GAMEPLAY_MAP_SIZE);
-        float x = cosf(angle) * distance;
-        float z = sinf(angle) * distance;
-        objects[i].position.x = x;
-        // This it the height of the object's origin.
-        objects[i].position.y = 10;
-        objects[i].position.z = z;
-        objects[i].visible = false;
+    for (int i = 0; i < OBJECT_COUNT; i++) {
+        float offsetX = ((i & 2) == 0) ? -mapQuadrant : mapQuadrant;
+        float offsetZ = ((i & 1) == 0) ? -mapQuadrant : mapQuadrant;
+
+        float x = offsetX + (float)GetRandomValue(minQuadrant, mapQuadrant - minQuadrant);
+        float z = offsetZ + (float)GetRandomValue(minQuadrant, mapQuadrant - minQuadrant);
+
+        objects[i].position = (Vector3){ x, OBJECT_Y, z };
+        objects[i].visible = true;
     }
 }
 
-bool DrawObjects(Model object, Camera *camera) {
-    Vector2 cameraPosition = (Vector2){ camera->position.x, camera->position.z };
-    float distance = 11.0f;
-
-    for (int i = 0; i < NUM_OBJECTS; i++) {
-        distance = Vector2Distance(
-            cameraPosition,
-            (Vector2) { objects[i].position.x, objects[i].position.z }
-        );
-
-        DrawModel(
-            object,
-            objects[i].position,
-            10.0f,
-            WHITE
-        );
+void AnimateObject(Object *objects) {
+    float time = GetTime();
+    float moveUpHeight = 10.0f;
+    float moveUpSpeed = 0.05f;
+    
+    float height = OBJECT_Y + moveUpHeight;
+    
+    if (objects->position.y < height) {
+        objects->position.y += moveUpSpeed;
+    } else {
+        objects->visible = false;
     }
-
-    return distance <= DISTANCE_THRESHOLD;
 }
+
+// Update hidden objects and check if they are within the distance threshold
+void UpdateObjects(Camera *camera) {
+    allObjectsFound = true;
+
+    for (int i = 0; i < OBJECT_COUNT; i++) {
+        if (objects[i].visible) {
+            float distance = Vector3Distance(camera->position, objects[i].position);
+
+            if (distance < DISTANCE_THRESHOLD) {
+                objects[i].isFound = true;
+            }
+
+            if (!objects[i].isFound) {
+                allObjectsFound = false;
+            }
+        }
+
+        if (objects[i].isFound) {
+            AnimateObject(&objects[i]);
+        }
+    }
+}
+
+void DrawObjects(Model object, Camera *camera) {
+    UpdateObjects(camera);
+
+    for (int i = 0; i < OBJECT_COUNT; i++) {
+        if (objects[i].visible) {
+            DrawModel(object, objects[i].position, 10.0f, WHITE);           
+        }
+    }
+}
+
