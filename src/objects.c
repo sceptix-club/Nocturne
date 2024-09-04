5acd5173593b4207a3f3f4fea680b6a4676f7344
JoelVCrasta <22b03.joelc@sjec.ac.in>
2024-09-04 12:57:52 +0530

    added marker when a object is found

--- src/objects.c
+++ src/objects.c
@@ -2,20 +2,28 @@
 
 #define MAP_SIZE 25 // Small map size for now
 #define MIN_SPAWN_RADIUS 25
-#define DISTANCE_THRESHOLD 10.0f
+#define DISTANCE_THRESHOLD 10.0
 #define OBJECT_COUNT 4
 #define OBJECT_Y 2.0f
-#define DISAPPEAR_TIME 3.0f
+#define FOUND_TIME 2.0f
 
+// Struct for hidden objects
 typedef struct {
+    int id;
     Vector3 position;
-    float rotation;
-    bool isVisible;
     bool isFound;
     float foundTime;
 } Object;
 
+// Struct for markers
+typedef struct {
+    float positionY;
+    float rotationY;
+    bool isVisible;
+} Marker;
+
 Object objects[OBJECT_COUNT];
+Marker markers[OBJECT_COUNT];
 bool allObjectsFound = false;
 
 Model ObjectModel(Shader lightShader) {
@@ -25,6 +33,13 @@ Model ObjectModel(Shader lightShader) {
     object.materials[0].shader = lightShader;
 
     return object;
+}
+
+Model MarkerModel() {
+    Mesh body = GenMeshCube(0.5f, 2.0f, 0.5f);
+    Model marker = LoadModelFromMesh(body);
+
+    return marker;
 }
 
 // Initialize hidden objects at random positions in 4 quadrants
@@ -40,25 +55,38 @@ void InitObjects() {
         float z = offsetZ + (float)GetRandomValue(minQuadrant, mapQuadrant - minQuadrant);
 
         objects[i] = (Object){
+            .id = i,
             .position = (Vector3){ x, OBJECT_Y, z },
-            .rotation = 0.0f,
-            .isVisible = true,
             .isFound = false,
             .foundTime = 0.0f
+        };
+
+        markers[i] = (Marker){
+            .positionY = 0.0f,
+            .rotationY = 0.0f,
+            .isVisible = false
         };
     }
 }
 
+/*
+    * Update the object's marker when found
+    * Rotate and oscillate the marker
+*/
 void ObjectFound(Object *obj) {
-    if (!obj->isFound) return;
+    float rotationSpeed = 0.5f;
+    float oscillationHeight = 6.0f;
+    float oscillationSpeed = 0.5f;
 
-    if (obj->foundTime == 0.0f) {
-        obj->foundTime = GetTime();
-    } else {
-        float elapsedTime = GetTime() - obj->foundTime;
-        if (elapsedTime > DISAPPEAR_TIME) {
-            obj->isVisible = false;
-        }
+    markers[obj->id].positionY = sinf((float)GetTime()) * oscillationSpeed + oscillationHeight;
+
+    markers[obj->id].rotationY += rotationSpeed;
+    if (markers[obj->id].rotationY >= 360.0f) {
+        markers[obj->id].rotationY = 0.0f;
+    }
+
+    if(GetTime() - obj->foundTime > FOUND_TIME){
+        markers[obj->id].isVisible = true;
     }
 }
 
@@ -69,20 +97,18 @@ void UpdateObjects(Camera *camera) {
     for (int i = 0; i < OBJECT_COUNT; i++) {
         Object *obj = &objects[i];
 
-        if (obj->isVisible) {
-            float distance = Vector3Distance(camera->position, obj->position);
+        float distance = Vector3Distance(camera->position, obj->position);
+        if (distance < DISTANCE_THRESHOLD && !obj->isFound) {
+            obj->isFound = true;
+            obj->foundTime = (float)GetTime();
+        }
 
-            if (distance < DISTANCE_THRESHOLD) {
-                obj->isFound = true;
-            }
+        if (!obj->isFound) {
+            allObjectsFound = false;
+        }
 
-            if (obj->isFound) {
-                ObjectFound(obj);
-            }
-
-            if (obj->isVisible) {
-                allObjectsFound = false;
-            }
+        if (obj->isFound){
+            ObjectFound(obj);
         }
     }
 }
@@ -91,11 +117,22 @@ void DrawObjects(Model object, Camera *camera) {
     UpdateObjects(camera);
 
     for (int i = 0; i < OBJECT_COUNT; i++) {
-        if (objects[i].isVisible) {
             DrawModelEx(object,
                         objects[i].position,
                         (Vector3){ 0.0f, 1.0f, 0.0f },
-                        objects[i].rotation,
+                        0.0f,
+                        Vector3One(),
+                        WHITE);
+    }
+}
+
+void DrawMarkers(Model marker) {
+    for (int i = 0; i < OBJECT_COUNT; i++) {
+        if (markers[i].isVisible) {
+            DrawModelEx(marker,
+                        (Vector3){ objects[i].position.x, markers[i].positionY, objects[i].position.z },
+                        (Vector3){ 0.0f, 1.0f, 0.0f },
+                        markers[i].rotationY,
                         Vector3One(),
                         WHITE);
         }
