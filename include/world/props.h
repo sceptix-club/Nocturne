03fd0641cce6eb351d8e10c9eace0fa337e2a0de
nearlynithin <nearlynithin@gmail.com>
2024-09-05 08:21:59 +0530

    draw bone in props header

--- include/world/props.h
+++ include/world/props.h
@@ -1,36 +1,9 @@
 #ifndef PROPS_H
 #define PROPS_H
 #define THRESHOLD 3.0f
-#include "grass.h"
 #include <raylib.h>
 
-Model bone;
-
-Model Bone(Shader light)
-{
-    bone = LoadModel("assets/models/tree1.obj");
-    bone.materials[0].shader = light;
-    return bone;
-}
-
-void DrawBone(bool draw,Vector3 position)
-{
-    if(draw)
-    {
-        for(int i=0 ;i<GRASSBLADE_COUNT; i++)
-        {
-            if(Vector3Distance(grassBlades[i].position,position) <= THRESHOLD)
-            {   //disable the grass around certain distance around the model
-                grassBlades[i].active = false;
-            }
-            else
-            {
-                grassBlades[i].active = true;
-            }
-
-        }
-        DrawModel(bone, position, 3.0f, RAYWHITE);
-    }
-}
+Model Bone(Shader light);
+void DrawBone(bool draw,Vector3 position);
 
 #endif
