448743767dbec322c2e2b1432513e846754ac30b
JoelVCrasta <22b03.joelc@sjec.ac.in>
2024-09-05 19:53:56 +0530

    added object one by one spawn but some bug is there

--- src/objects.c
+++ src/objects.c
@@ -1,28 +1,22 @@
 #include "world/objects.h"
 #include "utils/sequence.h"
 
-/*
-    * Small map size for now
-    * Increase MAP_SIZE for more object spread
-    * Keep MIN_SPAWN_RADIUS less than MAP_SIZE
-*/
-#define MAP_SIZE 40
-#define MIN_SPAWN_RADIUS 30
+#define MAP_SIZE 30
+#define MIN_SPAWN_RADIUS 25
 #define DISTANCE_THRESHOLD 6.0
 #define OBJECT_COUNT 4
 #define OBJECT_Y 0.0f
 #define OBJECT_SCALE (Vector3){ 3.0f, 3.0f, 3.0f }
 #define FOUND_TIME 2.0f
 
-// Struct for hidden objects
 typedef struct {
     int id;
     Vector3 position;
     bool isFound;
+    bool isNextToFind;
     float foundTime;
 } Object;
 
-// Struct for markers
 typedef struct {
     bool isVisible;
 } Marker;
@@ -36,48 +30,38 @@ static float markerPositionY = 0.0f;
 static float markerRotationY = 0.0f;
 int *seq;
 
-
-
 AllObjects ObjectModel(Shader lightShader) {
-    // Bone model
-    Model bone = LoadModel("assets/models/bone.obj");
-    bone.materials[0].shader = lightShader;
-
-    // Ball model
-    Model ball = LoadModel("assets/models/ball.obj");
-    ball.materials[0].shader = lightShader;
-
-    // Sign Board model
-    Model sign = LoadModel("assets/models/ball.obj");
-    sign.materials[0].shader = lightShader;
-
-    // Grave model
-    Model grave = LoadModel("assets/models/grave.obj");
-    grave.materials[0].shader = lightShader;
-
-    objectModel = (AllObjects){
-        .bone = bone,
-        .ball = ball,
-        .sign = sign,
-        .grave = grave
+    const char *modelPaths[OBJECT_COUNT] = {
+        "assets/models/bone.obj",
+        "assets/models/ball.obj",
+        "assets/models/sign.obj",
+        "assets/models/grave.obj"
     };
 
-    return objectModel;
+    Model models[OBJECT_COUNT];
+    for (int i = 0; i < OBJECT_COUNT; i++) {
+        models[i] = LoadModel(modelPaths[i]);
+        models[i].materials[0].shader = lightShader;
+    }
+
+    return (AllObjects) {
+        .bone = models[0],
+        .ball = models[1],
+        .sign = models[2],
+        .grave = models[3]
+    };
 }
 
 Model MarkerModel() {
     Mesh body = GenMeshCube(0.3f, 0.3f, 0.3f);
-    Model marker = LoadModelFromMesh(body);
-
-    return marker;
+    return LoadModelFromMesh(body);
 }
 
-// Initialize hidden objects at random positions in 4 quadrants
 void InitObjects() {
     float mapQuadrant = MAP_SIZE / 2.0f;
     float minQuadrant = MIN_SPAWN_RADIUS / 2.0f;
 
-    seq = GenerateRandomSequence(4, 0, 3);
+    seq = GenerateRandomSequence(OBJECT_COUNT, 0, OBJECT_COUNT - 1);
     if (!seq) {
         puts("Error: Sequence generation failed.");
         return;
@@ -87,13 +71,14 @@ void InitObjects() {
         float offsetX = (i & 2) ? mapQuadrant : -mapQuadrant;
         float offsetZ = (i & 1) ? mapQuadrant : -mapQuadrant;
 
-        float x = offsetX + (float)GetRandomValue(minQuadrant, mapQuadrant - minQuadrant);
-        float z = offsetZ + (float)GetRandomValue(minQuadrant, mapQuadrant - minQuadrant);
+        float x = offsetX + GetRandomValue(minQuadrant, mapQuadrant - minQuadrant);
+        float z = offsetZ + GetRandomValue(minQuadrant, mapQuadrant - minQuadrant);
 
         objects[i] = (Object){
             .id = i,
             .position = (Vector3){ x, OBJECT_Y, z },
             .isFound = false,
+            .isNextToFind = (i == seq[0]),
             .foundTime = 0.0f
         };
 
@@ -101,93 +86,73 @@ void InitObjects() {
     }
 }
 
-/*
-    * Update the object's marker when found
-    * Rotate and oscillate the marker
-*/
-void ObjectFound(Object *obj) {
+void UpdateMarker(Object *obj) {
     float time = GetTime();
     float rotationSpeed = 0.5f;
-    float height = 6.0f;
     float oscillationSpeed = 0.5f;
-
-    markerPositionY = sinf(time * 0.5f) * oscillationSpeed + height;
+    markerPositionY = sinf(time * 0.5f) * oscillationSpeed + 6.0f;
     markerRotationY = fmodf(markerRotationY + rotationSpeed, 360.0f);
 
-    if(GetTime() - obj->foundTime > FOUND_TIME){
+    if (time - obj->foundTime > FOUND_TIME) {
         markers[obj->id].isVisible = true;
     }
 }
 
-// Update hidden objects and check if they are within the distance threshold
 void UpdateObjects(Camera *camera) {
     allObjectsFound = true;
 
     for (int i = 0; i < OBJECT_COUNT; i++) {
         Object *obj = &objects[i];
-
         float distance = Vector3Distance(camera->position, obj->position);
+
         if (distance < DISTANCE_THRESHOLD && !obj->isFound) {
             obj->isFound = true;
-            obj->foundTime = (float)GetTime();
+            obj->foundTime = GetTime();
+
+            if (i + 1 < OBJECT_COUNT) {
+                objects[i + 1].isNextToFind = true;
+            }
+
         }
 
-        if (obj->isFound){
-            ObjectFound(obj);
+        if (obj->isFound) {
+            UpdateMarker(obj);
         } else {
             allObjectsFound = false;
         }
     }
 }
 
-void DrawObjects(AllObjects object, Camera *camera) {
+void DrawObjects(AllObjects objectsModels, Camera *camera) {
     UpdateObjects(camera);
 
-    DrawModelEx(
-        object.bone, 
-        (Vector3){ objects[seq[0]].position.x, OBJECT_Y, objects[seq[0]].position.z }, 
-        (Vector3){ 0.0f, 1.0f, 0.0f }, 
-        0.0f, 
-        OBJECT_SCALE, 
-        WHITE
-    );
+    Model objectModels[OBJECT_COUNT] = { objectsModels.bone, objectsModels.ball, objectsModels.sign, objectsModels.grave };
 
-    DrawModelEx(
-        object.ball, 
-        (Vector3){ objects[seq[1]].position.x, OBJECT_Y, objects[seq[1]].position.z }, 
-        (Vector3){ 0.0f, 1.0f, 0.0f }, 
-        0.0f, OBJECT_SCALE, 
-        WHITE
-    );
-
-    DrawModelEx(
-        object.sign, 
-        (Vector3){ objects[seq[2]].position.x, OBJECT_Y, objects[seq[2]].position.z }, 
-        (Vector3){ 0.0f, 1.0f, 0.0f }, 
-        0.0f, 
-        OBJECT_SCALE, 
-        WHITE
-    );
-
-    DrawModelEx(
-        object.grave, 
-        (Vector3){ objects[seq[3]].position.x, OBJECT_Y, objects[seq[3]].position.z }, 
-        (Vector3){ 0.0f, 1.0f, 0.0f }, 
-        0.0f, 
-        OBJECT_SCALE, 
-        WHITE
-    );
+    for (int i = 0; i < OBJECT_COUNT; i++) {
+        if (objects[seq[i]].isNextToFind) {
+            DrawModelEx(
+                objectModels[i], 
+                objects[seq[i]].position, 
+                (Vector3){ 0.0f, 1.0f, 0.0f }, 
+                0.0f, 
+                OBJECT_SCALE, 
+                WHITE
+            );
+        }
+    }
 }
 
 void DrawMarkers(Model marker) {
     for (int i = 0; i < OBJECT_COUNT; i++) {
         if (markers[i].isVisible) {
-            DrawModelEx(marker,
-                        (Vector3){ objects[i].position.x,markerPositionY, objects[i].position.z },
-                        (Vector3){ 0.0f, 1.0f, 0.0f },
-                        markerRotationY,
-                        Vector3One(),
-                        WHITE);
+            DrawModelEx(
+                marker, 
+                (Vector3){ objects[i].position.x, markerPositionY, objects[i].position.z }, 
+                (Vector3){ 0.0f, 1.0f, 0.0f }, 
+                markerRotationY, 
+                Vector3One(), 
+                WHITE
+            );
         }
     }
 }
