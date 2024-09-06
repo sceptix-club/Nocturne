8f507ffa930649337d500c479d5c88befd23ab21
nearlynithin <nearlynithin@gmail.com>
2024-09-06 20:16:18 +0530

    Changed the attenuation value to make the light brighter

--- src/shaders/lights.c
+++ src/shaders/lights.c
@@ -16,7 +16,7 @@ Shader SetLights()
     lightShader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(lightShader, "viewPos");
 
     int ambientLoc = GetShaderLocation(lightShader, "ambient");
-    float attenuation = 0.3f;
+    float attenuation = 0.1f;
     int attenuationLoc = GetShaderLocation(lightShader, "attenuationLoc");
 
     SetShaderValue(lightShader, ambientLoc, (float[4]){ 0.05f, 0.075f, 0.07f, 1.0f }, SHADER_UNIFORM_VEC4);
