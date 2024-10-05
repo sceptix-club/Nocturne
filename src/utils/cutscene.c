a1afe0ef9f0a99d6ca8bd63ef5b2e4527bbb4674
nearlynithin <nearlynithin@gmail.com>
2024-10-05 11:51:55 +0530

    cutscene implementation needs fixing

---
+++ src/utils/cutscene.c
@@ -0,0 +1,47 @@
+#include "utils/cutscene.h"
+#include <raylib.h>
+#include <stdio.h>
+
+Image cutsceneBg;
+Texture2D cutsceneBgTex;
+unsigned int cutsceneNextFrameOffset = 0;
+int cutsceneAnimFrames = 0, cutsceneCurrentFrame = 0, cutsceneFrameDelay = 2, cutsceneFrameCounter = 0;
+int cutsceneTotalFrames = 1590;
+int length;
+
+void initCutscene()
+{
+    cutsceneBg = LoadImageAnim("assets/cutscene/finalcutscene1.gif", &cutsceneAnimFrames);
+    // printf("cutsceneAnimeFrames : %d",cutsceneAnimFrames);
+    cutsceneBgTex = LoadTextureFromImage(cutsceneBg);
+    length = (GetMonitorHeight(GetCurrentMonitor())-cutsceneBgTex.height)/2;
+}
+
+int PlayCutScene(bool play, Music audio)
+{
+    if(play)
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        if(!IsMusicStreamPlaying(audio))
+            PlayMusicStream(audio);
+        printf("Cutscene Should be processing rn\n");
+        cutsceneFrameCounter++;
+        if(cutsceneFrameCounter >= cutsceneFrameDelay)
+        {
+            cutsceneCurrentFrame++;
+            if(cutsceneCurrentFrame >= cutsceneAnimFrames)
+            {
+                cutsceneCurrentFrame = 0;
+            }
+            cutsceneNextFrameOffset = cutsceneBg.width * cutsceneBg.height * 4 * cutsceneCurrentFrame;
+            UpdateTexture(cutsceneBgTex, ((unsigned char*)cutsceneBg.data) + cutsceneNextFrameOffset);
+            cutsceneFrameCounter = 0;
+        }
+        DrawTextureEx(cutsceneBgTex,(Vector2){0,0},0,2,WHITE);
+        EndDrawing();
+    }
+    
+    if(cutsceneCurrentFrame == cutsceneTotalFrames) return 1;
+    else return 0;
+}
